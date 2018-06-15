36d88692b99bc6a4312d13d5dc2dfed3033712dc
Alex Montgomerie <AlexMontgomerie@github.com>
2018-06-15 23:02:12 +0100

    improving process

--- process.php
+++ process.php
@@ -10,9 +10,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
     $u_name = $_POST["survey_name"]; //set PHP variables like this so we can use them anywhere in code below
     $u_email = $_POST["survey_email"];
     $u_text = $_POST["user_text"];
-    $q1_score = $_POST[""];
+    $q1_score = $_POST["q1q1_score"];
     $q1_tickbox = implode(', ', $_POST["q1_tickbox"]);
-    $q1_explain = $_POST[""];
+    $q1_explain = $_POST["q1_explain"];
 
     $mysqli = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
 
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
 
     $statement = $mysqli->prepare("INSERT INTO survey_data (q1_score, q1_tickbox, q1_explain) VALUES(?, ?, ?)"); //prepare sql insert query
     //bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
-    $statement->bind_param('iis', $q1_score, $q1_tickbox, $q1_explain); //bind values and execute insert query
+    $statement->bind_param('iss', $q1_score, $q1_tickbox, $q1_explain); //bind values and execute insert query
 
     if($statement->execute()){
         print "Hello " . $u_name . "!, your message has been saved!";
