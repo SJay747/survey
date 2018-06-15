ffba8a66b7021ba4589c6392870391a8c3438c6a
Alex Montgomerie <AlexMontgomerie@github.com>
2018-06-15 22:52:03 +0100

    process first test

--- process.php
+++ process.php
@@ -1,21 +1,30 @@
 <?php
+
+$mysql_host = "localhost";
+$mysql_username = "collabo_survey_user";
+$mysql_password = "e7TFZOzWxUj49itQ";
+$mysql_database = "collabo_survey";
+
 //process.php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
-    $u_name = $_POST["user_name"]; //set PHP variables like this so we can use them anywhere in code below
-    $u_email = $_POST["user_email"];
+    $u_name = $_POST["survey_name"]; //set PHP variables like this so we can use them anywhere in code below
+    $u_email = $_POST["survey_email"];
     $u_text = $_POST["user_text"];
-    
+    $q1_score = $_POST[""];
+    $q1_tickbox = implode(', ', $_POST["q1_tickbox"]);
+    $q1_explain = $_POST[""];
+
     $mysqli = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
-    
+
     //Output any connection error
     if ($mysqli->connect_error) {
         die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-    }   
-    
-    $statement = $mysqli->prepare("INSERT INTO users_data (user_name, user_email, user_message) VALUES(?, ?, ?)"); //prepare sql insert query
+    }
+
+    $statement = $mysqli->prepare("INSERT INTO survey_data (q1_score, q1_tickbox, q1_explain) VALUES(?, ?, ?)"); //prepare sql insert query
     //bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
-    $statement->bind_param('sss', $u_name, $u_email, $u_text); //bind values and execute insert query
-    
+    $statement->bind_param('iis', $q1_score, $q1_tickbox, $q1_explain); //bind values and execute insert query
+
     if($statement->execute()){
         print "Hello " . $u_name . "!, your message has been saved!";
     }else{
